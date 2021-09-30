8af96f67c1ee96ada4a7fe9c875dba3579216b78
Kelly Sovacool <kellysovacool@gmail.com>
2021-09-30 17:30:42 -0400

    Fix merge conflicts

--- sensspec.sh
+++ sensspec.sh
@@ -1,4 +1,5 @@
-#export PATH="bin/mothur-1.37.0/:$PATH"
+mothur_bin_path={$1: ?ERROR: provide a path to a mothur binary}
+export PATH="$mothur_bin_path:$PATH"#"bin/mothur-1.37.0/:$PATH"
 version=$(mothur -v | grep version | sed 's/^.*=//')
 outdir=results/mothur-${version}/
 listfile=mouse.no_header.list
@@ -6,7 +7,7 @@ namefile=mouse.ng.names
 distfile=mouse.ng.dist
 
 mkdir -p $outdir
-mothur "#set.logfile(name=mothur-"${version}".log); 
+mothur "#set.logfile(name=log/mothur-"${version}".log);
         set.dir(input=data/, output="${outdir}");
-        sens.spec(list="${listfile}", name="${namefile}", column="${distfile}", label=userLabel, cutoff=0.03) 
+        sens.spec(list="${listfile}", name="${namefile}", column="${distfile}", label=userLabel, cutoff=0.03)
         "
