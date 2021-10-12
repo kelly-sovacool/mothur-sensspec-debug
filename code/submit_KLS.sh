e4e5719d842c54d162f306861b6e62b13f77985a
Kelly Sovacool <kellysovacool@gmail.com>
2021-10-12 11:38:56 -0400

    Create slurm submission script

---
+++ code/submit_KLS.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+##################
+####  Slurm preamble
+
+#### #### ####  These are the most frequently changing options
+
+####  Job name
+#SBATCH --job-name=mothurss
+
+####  Request resources here
+####    These are typically, number of processors, amount of memory,
+####    an the amount of time a job requires.  May include processor
+####    type, too.
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem-per-cpu=500MB
+#SBATCH --time=96:00:00
+
+#SBATCH --output=log/slurm-%j_%x.out
+
+####  Slurm account and partition specification here
+####    These will change if you work on multiple projects, or need
+####    special hardware, like large memory nodes or GPUs.
+
+#SBATCH --account=pschloss0
+#SBATCH --partition=standard
+
+#### #### ####  These are the least frequently changing options
+
+####  Your e-mail address and when you want e-mail
+
+#SBATCH --mail-user=sovacool@umich.edu
+#SBATCH --mail-type=BEGIN,END
+
+snakemake -j 2
