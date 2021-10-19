b01194aa12dd35c72f8d272d5178baae6ffadf1b
Kelly Sovacool <kellysovacool@gmail.com>
2021-10-19 11:10:14 -0400

    Convert 'sobs' to double and rename to 'num_otus'

--- code/mutate_sensspec.R
+++ code/mutate_sensspec.R
@@ -1,9 +1,13 @@
 library(tidyverse)
 
+summary_dat <- read_tsv(snakemake@input[['summary']]) %>%
+    mutate(sobs = double(sobs) %>%
+    rename(num_otus = sobs)
+
 read_tsv(snakemake@input[['sensspec']]) %>%
     mutate(mothur_version = snakemake@wildcards[['version']],
            dataset = snakemake@wildcards[['dataset']],
            filetype = snakemake@wildcards[['filetype']],
            cluster_method = snakemake@wildcards[['method']]) %>%
-    bind_cols(read_tsv(snakemake@input[['summary']])) %>%
+    bind_cols(summary_dat) %>%
     write_tsv(snakemake@output[['tsv']])
