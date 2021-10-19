bd4abcc570fc15aa89f4f866434d0414491e84d5
Kelly Sovacool <kellysovacool@gmail.com>
2021-10-19 11:21:47 -0400

    Why is num_otus still sometimes char?

--- code/mutate_sensspec.R
+++ code/mutate_sensspec.R
@@ -1,13 +1,13 @@
 library(tidyverse)
 
 summary_dat <- read_tsv(snakemake@input[['summary']]) %>%
-    mutate(sobs = as.double(sobs)) %>%
     rename(num_otus = sobs)
 
 read_tsv(snakemake@input[['sensspec']]) %>%
+    bind_cols(summary_dat) %>%
     mutate(mothur_version = snakemake@wildcards[['version']],
            dataset = snakemake@wildcards[['dataset']],
            filetype = snakemake@wildcards[['filetype']],
-           cluster_method = snakemake@wildcards[['method']]) %>%
-    bind_cols(summary_dat) %>%
+           cluster_method = snakemake@wildcards[['method']],
+           num_otus = as.double(num_otus)) %>%
     write_tsv(snakemake@output[['tsv']])
