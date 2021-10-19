9ca8e2714ac0c43485ba66fdf40258aa586259d8
Kelly Sovacool <kellysovacool@gmail.com>
2021-10-19 11:28:03 -0400

    Try to fix it in post?

--- code/concat_tsv.R
+++ code/concat_tsv.R
@@ -1,5 +1,5 @@
 library(tidyverse)
 
 snakemake@input[['tsv']] %>%
-    map_dfr(read_tsv) %>%
+    map_dfr(function(x) { read_tsv(x) %>% mutate(num_otus = as.double(num_otus))}) %>%
     write_tsv(snakemake@output[['tsv']])
