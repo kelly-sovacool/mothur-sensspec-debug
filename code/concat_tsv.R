8d6cf6c139a095348edce7ffc3b1a553f07885f7
Kelly Sovacool <kellysovacool@gmail.com>
2021-10-19 11:29:04 -0400

    uh npshannon is a double tooo....

--- code/concat_tsv.R
+++ code/concat_tsv.R
@@ -1,5 +1,8 @@
 library(tidyverse)
 
 snakemake@input[['tsv']] %>%
-    map_dfr(function(x) { read_tsv(x) %>% mutate(num_otus = as.double(num_otus))}) %>%
+    map_dfr(function(x) { read_tsv(x) %>%
+                            mutate(num_otus = as.double(num_otus),
+                                   npshannon = as.double(npshannon))
+                        }) %>%
     write_tsv(snakemake@output[['tsv']])
